359c4fc0448e1e53ed0365f97c0dfe078c3e5803
Ionela Alexandra <sauionela51@gmail.com>
2024-03-19 14:44:14 +0200

    added pacient register

--- medical_clinic_BACKEND/src/main/java/com/example/medical_clinic_BACKEND/Controller/PacientController.java
+++ medical_clinic_BACKEND/src/main/java/com/example/medical_clinic_BACKEND/Controller/PacientController.java
@@ -1,42 +1,66 @@
 package com.example.medical_clinic_BACKEND.Controller;
 
+import com.example.medical_clinic_BACKEND.Model.Consultatie;
 import com.example.medical_clinic_BACKEND.Model.Pacient;
+import com.example.medical_clinic_BACKEND.Service.ConsultatieService;
 import com.example.medical_clinic_BACKEND.Service.PacientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "api/pacient")
 public class PacientController {
     private final PacientService pacientService;
+    private final ConsultatieService consultatieService;
 
     @Autowired
-    public PacientController(PacientService pacientService) {
+    public PacientController(PacientService pacientService, ConsultatieService consultatieService) {
         this.pacientService = pacientService;
+        this.consultatieService = consultatieService;
     }
 
     @GetMapping
-    public List<Pacient> getPacienti() {
-        return pacientService.getPacienti();
+    public List<Pacient> getPacienti(@RequestParam(required = false) Long medicId) {
+        if (medicId == null) return pacientService.getPacienti();
+        else {
+            List<Consultatie> consultatii = consultatieService.findByMedicId(medicId);
+            if (consultatii != null) {
+                return consultatii.stream().map(Consultatie::getPacient).distinct().collect(Collectors.toList());
+            } else {
+                return new ArrayList<>();
+            }
+        }
+    }
+
+
+    @GetMapping(path = "{pacientId}")
+    public Pacient getPacientById(@PathVariable("pacientId") Long pacientId) {
+        return pacientService.getPacientById(pacientId);
     }
 
     @PostMapping
-    public void addPacient(@RequestBody Pacient pacient) {
+    public ResponseEntity<Pacient> addPacient(@RequestBody Pacient pacient) {
         pacientService.addPacient(pacient);
+        return ResponseEntity.ok(pacient);
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/login")
-    public String loginPacient(@RequestBody Map<String, String> credentials) {
+    public ResponseEntity<Pacient> loginPacient(@RequestBody Map<String, String> credentials) {
         String emailPacient = credentials.get("emailPacient");
         String parolaPacient = credentials.get("parolaPacient");
         if (pacientService.isValidCredentials(emailPacient, parolaPacient)) {
-            return "Login successful!";
+            Pacient pacient = pacientService.findByEmail(emailPacient);
+            return ResponseEntity.ok(pacient);
         } else {
-            return "Invalid credentials. Please try again.";
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 
@@ -46,7 +70,7 @@ public class PacientController {
     }
 
     @PutMapping(path = "{pacientId}")
-    public void updatePacient(@PathVariable("pacientId") Long pacientId, @RequestParam(required = false) String numePacient, @RequestParam(required = false) String prenumePacient, @RequestParam(required = false) String emailPacient, @RequestParam(required = false) String telefonPacient, @RequestParam(required = false) String parolaPacient, @RequestParam(required = false) String cnpPacient, @RequestParam(required = false) Character asigurat, @RequestParam(required = false) Character abonamentPacient,@RequestParam(required = false) Double inaltimePacient, @RequestParam(required = false) Double greutatePacient, @RequestParam(required = false) Integer varstaPacient) {
+    public void updatePacient(@PathVariable("pacientId") Long pacientId, @RequestParam(required = false) String numePacient, @RequestParam(required = false) String prenumePacient, @RequestParam(required = false) String emailPacient, @RequestParam(required = false) String telefonPacient, @RequestParam(required = false) String parolaPacient, @RequestParam(required = false) String cnpPacient, @RequestParam(required = false) Character asigurat, @RequestParam(required = false) Character abonamentPacient, @RequestParam(required = false) Double inaltimePacient, @RequestParam(required = false) Double greutatePacient, @RequestParam(required = false) Integer varstaPacient) {
         pacientService.updatePacient(pacientId, numePacient, prenumePacient, cnpPacient, telefonPacient, emailPacient, parolaPacient, inaltimePacient, greutatePacient, abonamentPacient, asigurat, varstaPacient);
     }
 }
---
+++ medical_clinic_FRONTEND/my-app/src/pages/PatientRegister.js
@@ -0,0 +1,186 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import { useUser } from '../user/UserContext';
+
+
+function PatientRegister() {
+    const [emailPacient, setEmailPacient] = useState('');
+    const [parolaPacient, setParolaPacient] = useState('');
+    const [numePacient, setNumePacient] = useState('');
+    const [prenumePacient, setPrenumePacient] = useState('');
+    const [cnpPacient, setCnpPacient] = useState('');
+    const [telefonPacient, setTelefonPacient] = useState('');
+    const [dataNasterePacient, setDataNasterePacient] = useState('');
+    const [sexPacient, setSexPacient] = useState('Masculin');
+    const [greutatePacient, setGreutatePacient] = useState('');
+    const [inaltimePacient, setInaltimePacient] = useState('');
+    const [varstaPacient, setVarstaPacient] = useState('');
+    const [asigurat, setAsigurat] = useState('Y');
+    const [abonamentPacient, setAbonamentPacient] = useState('Y');
+    const navigate = useNavigate();
+    const { setUser } = useUser();  // get setUser from the user context
+
+
+    const handleRegistration = async () => {
+        try {
+            const newUser = {
+                numePacient,
+                prenumePacient,
+                cnpPacient,
+                dataNasterePacient,
+                telefonPacient,
+                emailPacient,
+                parolaPacient,
+                sexPacient,
+                greutatePacient,
+                inaltimePacient,
+                varstaPacient,
+                asigurat,
+                abonamentPacient
+            };
+
+            console.log(newUser.asigurat);
+            console.log(newUser.abonamentPacient);
+            console.log(newUser.numePacient);
+
+            const response = await fetch('http://localhost:8081/api/pacient', {
+                method: 'POST', headers: {
+                    'Content-Type': 'application/json'
+                }, body: JSON.stringify(newUser)
+            });
+
+            if (response.ok) {
+                console.log('User registered successfully');
+                const user = await response.json();  // parse the response as JSON
+                setUser(user);  // set the user's data in the user context
+                navigate('/pacient/dashboard');
+            } else {
+                console.log('Registration failed');
+            }
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    };
+
+    return (<div className="pt-5">
+            <div className="max-w-md mx-auto bg-white p-5 rounded shadow-md">
+                <h2 className="text-center text-2xl font-bold text-gray-800">Register</h2>
+                <label className="block mb-2">
+                    Nume:
+                    <input
+                        type="text"
+                        value={numePacient}
+                        onChange={e => setNumePacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Prenume:
+                    <input
+                        type="text"
+                        value={prenumePacient}
+                        onChange={e => setPrenumePacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    CNP:
+                    <input
+                        type="text"
+                        value={cnpPacient}
+                        onChange={e => setCnpPacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Data nasterii:
+                    <input
+                        type="date"
+                        value={dataNasterePacient}
+                        onChange={e => setDataNasterePacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Sex:
+                    <select value={sexPacient} onChange={e => setSexPacient(e.target.value)}>
+                        <option value="masculin">Masculin</option>
+                        <option value="feminin">Feminin</option>
+                    </select>
+                </label>
+                <label className="block mb-2">
+                    Greutate:
+                    <input
+                        type="number"
+                        value={greutatePacient}
+                        onChange={e => setGreutatePacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Inaltime:
+                    <input
+                        type="number"
+                        value={inaltimePacient}
+                        onChange={e => setInaltimePacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Varsta:
+                    <input
+                        type="number"
+                        value={varstaPacient}
+                        onChange={e => setVarstaPacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Asigurat:
+                    <select value={asigurat} onChange={e => setAsigurat(e.target.value)}>
+                        <option value="Y">Da</option>
+                        <option value="N">Nu</option>
+                    </select>
+                </label>
+                <label className="block mb-2">
+                    Abonament:
+                    <select value={abonamentPacient} onChange={e => setAbonamentPacient(e.target.value)}>
+                        <option value="Y">Da</option>
+                        <option value="N">Nu</option>
+                    </select>
+                </label>
+                <label className="block mb-2">
+                    Telefon:
+                    <input
+                        type="phone"
+                        value={telefonPacient}
+                        onChange={e => setTelefonPacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Email:
+                    <input
+                        type="email"
+                        value={emailPacient}
+                        onChange={e => setEmailPacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <label className="block mb-2">
+                    Parola:
+                    <input
+                        type="password"
+                        value={parolaPacient}
+                        onChange={e => setParolaPacient(e.target.value)}
+                        className="w-full px-2 py-1 mb-4 border border-gray-300"
+                    />
+                </label>
+                <button onClick={handleRegistration}
+                        className="w-full px-4 py-2 bg-gray-800 text-white border-none cursor-pointer">Register
+                </button>
+            </div>
+        </div>);
+}
+
+export default PatientRegister;
