6e04aedf3aa77e292c7de91c43af5a75745bdce0
Ionela Alexandra <sauionela51@gmail.com>
2024-03-31 17:54:49 +0300

    Update Prices.js

--- medical_clinic_FRONTEND/my-app/src/pages/Prices.js
+++ medical_clinic_FRONTEND/my-app/src/pages/Prices.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import PatientMenu from "../components/PatientMenu";
 
-const ConsultatiePreturi = () => {
+const Prices = () => {
     const [consultatii, setConsultatii] = useState([]);
 
     useEffect(() => {
@@ -17,24 +17,52 @@ const ConsultatiePreturi = () => {
 
     console.log(consultatii)
 
-    return (<div className="p-6">
+    return (
+        <div className="p-6">
             <PatientMenu/>
             <h1 className="text-3xl font-bold mb-4">Preturi Consultatii</h1>
             <div className="flex">
-                <div className="bg-white p-4 rounded shadow w-1/2 mr-2">
+                <div className="bg-white p-4 rounded shadow w-full mr-2">
                     <h2 className="text-2xl font-bold mb-2">Preturi Cu Abonament</h2>
-                    {consultatii.map(consultatie => (<div key={consultatie.id}>
-                            <h3>{consultatie.numeConsultatie} {consultatie.pretCuAbonament}</h3>
-                        </div>))}
+                    <table className="table-auto border-collapse border border-blue-800 w-full">
+                        <thead>
+                        <tr>
+                            <th className="border border-blue-600 p-2">Consultatie</th>
+                            <th className="border border-blue-600 p-2">Pret</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {consultatii.map(consultatie => (
+                            <tr key={consultatie.id}>
+                                <td className="border border-blue-600 p-2">{consultatie.numeConsultatie}</td>
+                                <td className="border border-blue-600 p-2">{consultatie.pretCuAbonament}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
-                <div className="bg-white p-4 rounded shadow w-1/2 ml-2">
+                <div className="bg-white p-4 rounded shadow w-full ml-2">
                     <h2 className="text-2xl font-bold mb-2">Preturi Fara Abonament</h2>
-                    {consultatii.map(consultatie => (<div key={consultatie.id}>
-                            <h3>{consultatie.numeConsultatie} {consultatie.pretFaraAbonament}</h3>
-                        </div>))}
+                    <table className="table-auto border-collapse border border-blue-800 w-full">
+                        <thead>
+                        <tr>
+                            <th className="border border-blue-600 p-2">Consultatie</th>
+                            <th className="border border-blue-600 p-2">Pret</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {consultatii.map(consultatie => (
+                            <tr key={consultatie.id}>
+                                <td className="border border-blue-600 p-2">{consultatie.numeConsultatie}</td>
+                                <td className="border border-blue-600 p-2">{consultatie.pretFaraAbonament}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
-        </div>);
+        </div>
+    );
 };
 
-export default ConsultatiePreturi;
+export default Prices;
