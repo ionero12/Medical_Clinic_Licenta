c9ccbf6a6f0bab004efc770895f429f98ccc591d
Ionela Alexandra <sauionela51@gmail.com>
2024-04-16 20:20:07 +0300

    added disease message and specialization

--- medical_clinic_BACKEND/src/main/java/com/example/medical_clinic_BACKEND/Controller/SymptomController.java
+++ medical_clinic_BACKEND/src/main/java/com/example/medical_clinic_BACKEND/Controller/SymptomController.java
@@ -7,14 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import weka.classifiers.trees.J48;
-import weka.core.Instances;
-import weka.core.SerializationHelper;
-import weka.core.Instance;
-import weka.core.DenseInstance;
+import weka.core.*;
 import weka.core.converters.CSVLoader;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/pacient/chestionar")
@@ -31,33 +31,54 @@ public class SymptomController {
 
         // Load training data
         CSVLoader loader = new CSVLoader();
-        loader.setSource(new File("D:\\Facultate\\Licenta\\Medical_Clinic_Licenta\\medical_clinic_BACKEND\\src\\main\\java\\com\\example\\medical_clinic_BACKEND\\Machine_Learning\\training_data.csv"));
+        loader.setSource(new File("D:\\Facultate\\Licenta\\Medical_Clinic_Licenta\\medical_clinic_BACKEND\\src\\main\\java\\com\\example\\medical_clinic_BACKEND\\Machine_Learning\\training_data1.csv"));
         this.trainData = loader.getDataSet();
         this.trainData.setClassIndex(this.trainData.numAttributes() - 1);
     }
 
     @PostMapping
     public ResponseEntity<String> receiveSymptoms(@RequestBody Symptoms symptoms) throws Exception {
+        Map<String, List<String>> diseasesBySpecialization = new HashMap<>();
+        diseasesBySpecialization.put("Dermatologie", Arrays.asList("Acnee", "Psoriazis", "Impetigo", "Infectie fungica"));
+        diseasesBySpecialization.put("Gastroenterologie", Arrays.asList("Boala de reflux gastroesofagian", "Colestaza cronica", "Boala ulcerului peptic", "Gastroenterita", "Hepatita A", "Hepatita B", "Hepatita C", "Hepatita D", "Hepatita E", "Hepatita alcoolica"));
+        diseasesBySpecialization.put("Cardiologie", Arrays.asList("Hipertensiune", "Atac de cord", "Varice"));
+        diseasesBySpecialization.put("Endocrinologie", Arrays.asList("Diabet", "Hipotiroidism", "Hipertiroidism", "Hipoglicemie"));
+        diseasesBySpecialization.put("Reumatologie", Arrays.asList("Osteoartrita", "Artrita"));
+        diseasesBySpecialization.put("Neurologie", Arrays.asList("Migrena", "Spondiloza cervicala", "Paralizie", "Vertij pozițional paroxistic"));
+        diseasesBySpecialization.put("Infectioase", Arrays.asList("HIV", "Icter", "Malarie", "Varicela", "Dengue", "Tifoida", "Tuberculoza", "Raceala comuna", "Pneumonie"));
+        diseasesBySpecialization.put("Urologie", List.of("Infectii ale tractului urinar"));
+        diseasesBySpecialization.put("Pneumologie", List.of("Astm bronsic"));
+        diseasesBySpecialization.put("Hematologie", List.of("Hemoroizi dimorfi"));
+        diseasesBySpecialization.put("Alergologie", List.of("Alergie"));
+        diseasesBySpecialization.put("Farmacologie", List.of("Reactie la medicamente"));
+
         if (symptoms != null && symptoms.getSymptoms() != null) {
-            // Check if symptoms object and symptoms map are not null
             System.out.println("Received Symptoms: " + symptoms);
-            // Create a new instance for the received symptoms
             Instance newInstance = new DenseInstance(symptoms.getSymptoms().size());
-            // Set the values of the instance to the received symptoms
             for (Map.Entry<String, Integer> entry : symptoms.getSymptoms().entrySet()) {
-                newInstance.setValue(trainData.attribute(entry.getKey()), entry.getValue());
+                Attribute att = trainData.attribute(entry.getKey());
+                if (att != null) {
+                    newInstance.setValue(att, entry.getValue());
+                } else {
+                    System.out.println("Atributul nu există în trainData: " + entry.getKey());
+                }
             }
-            // Create a new Instances object with the same structure as trainData and add newInstance to it
             Instances instances = new Instances(trainData, 0);
             instances.add(newInstance);
             instances.setClassIndex(instances.numAttributes() - 1);
-            // Predict the disease
             double predictedClass = model.classifyInstance(instances.firstInstance());
             String predictedDisease = trainData.classAttribute().value((int) predictedClass);
-            return ResponseEntity.ok(predictedDisease);
+
+            for (Map.Entry<String, List<String>> entry : diseasesBySpecialization.entrySet()) {
+                if (entry.getValue().contains(predictedDisease)) {
+                    return ResponseEntity.ok("Boala care se potriveste cu simpotomele tale este " +  predictedDisease + ". Te rugam sa consulti un medic de la specializarea " + entry.getKey() + ".");
+                }
+            }
+
+            return ResponseEntity.ok("Boala prezisa: " + predictedDisease);
         } else {
             System.out.println("Received Symptoms: null");
-            return ResponseEntity.badRequest().build(); // Return bad request if symptoms are null
+            return ResponseEntity.badRequest().build();
         }
     }
 }
--- medical_clinic_FRONTEND/my-app/src/pages/Questionnaire.js
+++ medical_clinic_FRONTEND/my-app/src/pages/Questionnaire.js
@@ -1,9 +1,12 @@
 import React, {useState} from "react";
 import axios from 'axios';
 import PatientMenu from "../components/PatientMenu";
+import DiseaseModal from "../components/DiseaseModal";
 
 
 const Questionnaire = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [responseData, setResponseData] = useState(null);
     const [symptoms, setSymptoms] = useState({symptoms: {}});
 
     const handleSymptomChange = (event) => {
@@ -25,54 +28,60 @@ const Questionnaire = () => {
             }
         })
             .then(response => {
-                // Handle successful response
                 console.log('Response:', response.data);
-                // Further processing of response data if needed
+                setResponseData(response.data);
+                setModalOpen(true);
             })
             .catch(error => {
-                // Handle error
                 console.error('Error:', error);
             });
     };
 
-    const symptomNames = ["fatigue", "vomiting", "high_fever", "loss_of_appetite", "nausea", "headache", "abdominal_pain", "yellowish_skin", "yellowing_of_eyes", "chills", "skin_rash", "chest_pain", "sweating", "itching", "dark_urine", "cough", "diarrhoea", "irritability", "muscle_pain", "excessive_hunger", "weight_loss", "mild_fever", "loss_of_balance", "blurred_and_distorted_vision", "dizziness", "abnormal_menstruation", "fast_heart_rate", "constipation", "neck_pain", "obesity", "burning_micturition", "increased_appetite", "runny_nose", "cold_hands_and_feets"];
+    const symptomNames = ["mancarime", "eruptii_cutanate", "stranut_continuu", "frisoane", "dureri_articulare", "durere_de_stomac", "aciditate", "varsaturi", "mictiune_dureroasa", "oboseala", "schimbari_de_stare", "pierdere_in_greutate", "neliniste", "letargie", "tuse", "febra_ridicata", "dispnee", "transpiratii", "indigestie", "durere_de_cap", "piele_galbuie", "urina_inchisa_la_culoare", "greata", "pierderea_apetitului", "durere_de_spate", "constipatie", "durere_abdominala", "diaree", "febra_usoara", "ochi_galbui", "ganglioni_limfatici_umflati", "stare_de_rau", "vedere_incetosata", "flegma", "nas_infundat", "durere_in_piept", "ritm_cardiac_crescut", "durere_in_gat", "ameteala", "obezitate", "apetit_crescut", "slabiciune_musculara", "gat_intepenit", "pierderea_echilibrului", "depresie", "iritabilitate", "durere_de_muschi", "pete_rosii_pe_corp", "menstruatii_anormale", "istoric_de_familie", "durere_la_mers"];
 
     return (<div className="pt-5">
         <PatientMenu/>
         <div className="max-w-md mx-auto bg-white p-5 rounded shadow-md text-center">
-                <h2 className="text-center text-2xl font-bold text-gray-800 pb-8">Symptoms Form</h2>
-                <form onSubmit={handleSubmit}>
-                    {symptomNames.map(symptom => (<div key={symptom} className="mb-4">
-                            <label className="mb-2 text-gray-700">
-                                {symptom.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")}:
-                            </label>
-                            <div className="">
-                                <label className="mr-4">
-                                    <input
-                                        type="radio"
-                                        name={symptom}
-                                        value="1"
-                                        checked={symptoms.symptoms[symptom] === 1}
-                                        onChange={handleSymptomChange}
-                                        className="mr-2 text-blue-500 focus:ring-blue-500"
-                                    /> Yes
-                                </label>
-                                <label>
-                                    <input
-                                        type="radio"
-                                        name={symptom}
-                                        value="0"
-                                        checked={symptoms.symptoms[symptom] === 0}
-                                        onChange={handleSymptomChange}
-                                        className="mr-2 text-blue-500 focus:ring-blue-500"
-                                    /> No
-                                </label>
-                            </div>
-                    </div>))}
+            <h2 className="text-center text-2xl font-bold text-gray-800 pb-8">Symptoms Form</h2>
+            <form onSubmit={handleSubmit}>
+                {symptomNames.map(symptom => (<div key={symptom} className="mb-4">
+                    <label className="mb-2 text-gray-700">
+                        {symptom.split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")}:
+                    </label>
+                    <div className="">
+                        <label className="mr-4">
+                            <input
+                                type="radio"
+                                name={symptom}
+                                value="1"
+                                checked={symptoms.symptoms[symptom] === 1}
+                                onChange={handleSymptomChange}
+                                className="mr-2 text-blue-500 focus:ring-blue-500"
+                            /> Da
+                        </label>
+                        <label>
+                            <input
+                                type="radio"
+                                name={symptom}
+                                value="0"
+                                checked={symptoms.symptoms[symptom] === 0}
+                                onChange={handleSymptomChange}
+                                className="mr-2 text-blue-500 focus:ring-blue-500"
+                            /> Nu
+                        </label>
+                    </div>
+                </div>))}
+                <div className="flex justify-center">
                     <input type="submit" value="Submit"
-                           className="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"/>
-                </form>
-            </div>
+                           className="block mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "/>
+                </div>
+            </form>
+        </div>
+        <div>
+            <DiseaseModal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+                {responseData}
+            </DiseaseModal>
+        </div>
     </div>);
 }
 
