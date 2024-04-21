0ca4739eafdd36bd9cfa81b64b44e4f87f93b49c
Ionela Alexandra <sauionela51@gmail.com>
2024-04-21 13:40:22 +0300

    improved the way an appointment is made

--- medical_clinic_FRONTEND/my-app/src/pages/MedicAppointments.js
+++ medical_clinic_FRONTEND/my-app/src/pages/MedicAppointments.js
@@ -4,6 +4,11 @@ import MedicMenu from '../components/MedicMenu';
 import AppointmentMedic from '../components/AppointmentMedic';
 import axios from "axios";
 import Modal from "react-modal";
+import 'animate.css/animate.min.css';
+import {faEdit, faPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const MedicAppointments = () => {
@@ -12,15 +17,14 @@ const MedicAppointments = () => {
     const [pastAppointments, setPastAppointments] = useState([]);
     const [upcomingAppointments, setUpcomingAppointments] = useState([]);
 
-    const {user} = useUser(); // get user from the user context
-    const idMedic = user ? user.userData.idMedic : null; // get patient ID from the user
+    const {user} = useUser();
+    const idMedic = user ? user.userData.idMedic : null;
 
-    const [idPacient, setIdPacient] = useState('');
-    const [cnpPacient, setCnpPacient] = useState(''); // New state for CNP
+    const [cnpPacient, setCnpPacient] = useState('');
 
     const [numeConsultatie, setNumeConsultatie] = useState('');
     const [selectedDate, setSelectedDate] = useState('');
-    const [selectedHour, setSelectedHour] = useState('08:00'); // Default to 08:00
+    const [selectedHour, setSelectedHour] = useState('08:00');
     const [idConsultatie, setIdConsultatie] = useState('');
 
     const [addModalIsOpen, setAddModalIsOpen] = useState(false);
@@ -45,6 +49,19 @@ const MedicAppointments = () => {
     const handleAddAppointment = async (event) => {
         event.preventDefault();
         console.log('Adding appointment:', idConsultatie, selectedDate, selectedHour);
+
+        const date = new Date(selectedDate);
+        const currentDate = new Date();
+
+        if (date < currentDate) {
+            toast.error('Nu se pot adauga programări la o dată din trecut');
+            return;
+        }
+        if (date.getDay() === 6 || date.getDay() === 0) {
+            toast.error('Nu se pot face programări în weekend');
+            return;
+        }
+
         const newAppointment = {
             pacient: {
                 cnpPacient
@@ -66,6 +83,19 @@ const MedicAppointments = () => {
     const handleUpdateAppointment = async (event) => {
         event.preventDefault();
         console.log('Updating appointment:', idConsultatie, selectedDate, selectedHour);
+
+        const date = new Date(selectedDate);
+        const currentDate = new Date();
+
+        if (date < currentDate) {
+            toast.error('Nu se pot actualiza programări la o dată din trecut');
+            return;
+        }
+        if (date.getDay() === 6 || date.getDay() === 0) {
+            toast.error('Nu se pot face programări în weekend');
+            return;
+        }
+
         try {
             const response = await axios.put(`http://localhost:8081/api/consultatie/${idConsultatie}`, null, {
                 params: {
@@ -124,116 +154,129 @@ const MedicAppointments = () => {
     }
 
 
-    return (<div className="p-6">
-        <MedicMenu/>
-        <h1 className="text-3xl font-bold mb-4">Programari</h1>
-        <div className="flex flex-col md:flex-row">
-            <div className="bg-white p-4 rounded shadow w-full md:w-1/2 mr-2 mb-4 md:mb-0">
-                <h2 className="text-2xl font-bold mb-2">Programari anterioare</h2>
-                <ul>
-                    {pastAppointments.map(appointment => (
-                        <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
-                            <AppointmentMedic appointment={appointment}/>
-                        </div>))}
-                </ul>
+    return (<div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                draggable
+                theme="light"
+            />
+            <div className="p-6">
+                <MedicMenu/>
+                <div className="flex flex-col md:flex-row mt-4">
+                    <div className="bg-white p-4 rounded shadow w-full md:w-1/2 mr-2 mb-4 md:mb-0">
+                        <h2 className="text-2xl font-bold mb-2">Programari anterioare</h2>
+                        <ul>
+                            {pastAppointments.map(appointment => (
+                                <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
+                                    <AppointmentMedic appointment={appointment}/>
+                                </div>))}
+                        </ul>
+                    </div>
+                    <div className="bg-white p-4 rounded shadow w-full md:w-1/2">
+                        <button
+                            onClick={openAddModal}
+                            className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-3 rounded mt-4">
+                            Adauga programare <FontAwesomeIcon icon={faPlus}/>
+                        </button>
+                        <Modal
+                            isOpen={addModalIsOpen}
+                            onRequestClose={closeAddModal}
+                            contentLabel="Add AppointmentMedic"
+                            className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center animate__animated animate__zoomIn"
+                        >
+                            <form onSubmit={handleAddAppointment} className="flex flex-col">
+                                <label className="mb-2">
+                                    CNP-ul pacientului:
+                                    <input type="text" value={cnpPacient}
+                                           onChange={e => setCnpPacient(e.target.value)} required
+                                           className="mt-1"/>
+                                </label>
+                                <label className="mb-2">
+                                    Numele consultatiei:
+                                    <input type="text" value={numeConsultatie}
+                                           onChange={e => setNumeConsultatie(e.target.value)} required
+                                           className="mt-1"/>
+                                </label>
+                                <label className="mb-2">
+                                    Selectati data:
+                                    <input
+                                        type="date"
+                                        value={selectedDate}
+                                        onChange={(e) => setSelectedDate(e.target.value)}
+                                        required
+                                    />
+                                </label>
+                                <label className="mb-2">
+                                    Selectati ora:
+                                    <select
+                                        value={selectedHour}
+                                        onChange={(e) => setSelectedHour(e.target.value)}
+                                        required
+                                    >
+                                        {hoursOptions}
+                                    </select>
+                                </label>
+                                <input type="submit" value="Submit"
+                                       className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
+                            </form>
+                        </Modal>
+                        <h2 className="text-2xl font-bold mb-2">Programari viitoare</h2>
+                        <ul>
+                            {upcomingAppointments.map(appointment => (
+                                <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
+                                    <AppointmentMedic appointment={appointment}/>
+                                    <button
+                                        onClick={() => {
+                                            setIdConsultatie(appointment.idConsultatie);
+                                            openEditModal();
+                                        }}
+                                        className="bg-sky-500 hover:bg-sky-600 text-white rounded px-2.5 py-2 transition duration-200">
+                                        Update <FontAwesomeIcon icon={faEdit}/>
+                                    </button>
+                                    <Modal
+                                        isOpen={editModalIsOpen}
+                                        onRequestClose={closeEditModal}
+                                        contentLabel="Update AppointmentMedic"
+                                        className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center animate__animated animate__zoomIn"
+                                    >
+                                        <form onSubmit={handleUpdateAppointment} className="flex flex-col">
+                                            <label className="mb-2">
+                                                Selectati data:
+                                                <input
+                                                    type="date"
+                                                    value={selectedDate}
+                                                    onChange={(e) => setSelectedDate(e.target.value)}
+                                                    required
+                                                />
+                                            </label>
+                                            <label className="mb-2">
+                                                Selectati ora:
+                                                <select
+                                                    value={selectedHour}
+                                                    onChange={(e) => setSelectedHour(e.target.value)}
+                                                    required
+                                                >
+                                                    {hoursOptions}
+                                                </select>
+                                            </label>
+                                            <input type="submit" value="Update"
+                                                   className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
+                                        </form>
+                                    </Modal>
+                                    <button onClick={() => deleteAppointment(appointment.idConsultatie)}
+                                            className="bg-red-500 text-white rounded px-2.5 py-2 hover:bg-red-700 ml-5 transition duration-200">
+                                        Delete <FontAwesomeIcon icon={faTrash}/>
+                                    </button>
+                                </div>))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="bg-white p-4 rounded shadow w-full md:w-1/2">
-                <button
-                    onClick={openAddModal}
-                    className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-3 rounded mt-4">Adauga programare
-                </button>
-                <Modal
-                    isOpen={addModalIsOpen}
-                    onRequestClose={closeAddModal}
-                    contentLabel="Add AppointmentMedic"
-                    className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center"
-                >
-                    <form onSubmit={handleAddAppointment} className="flex flex-col">
-                        <label className="mb-2">
-                            CNP-ul pacientului:
-                            <input type="text" value={cnpPacient}
-                                   onChange={e => setCnpPacient(e.target.value)} required
-                                   className="mt-1"/>
-                        </label>
-                        <label className="mb-2">
-                            Numele consultatiei:
-                            <input type="text" value={numeConsultatie}
-                                   onChange={e => setNumeConsultatie(e.target.value)} required
-                                   className="mt-1"/>
-                        </label>
-                        <label className="mb-2">
-                            Selectati data:
-                            <input
-                                type="date"
-                                value={selectedDate}
-                                onChange={(e) => setSelectedDate(e.target.value)}
-                                required
-                            />
-                        </label>
-                        <label className="mb-2">
-                            Selectati ora:
-                            <select
-                                value={selectedHour}
-                                onChange={(e) => setSelectedHour(e.target.value)}
-                                required
-                            >
-                                {hoursOptions}
-                            </select>
-                        </label>
-                        <input type="submit" value="Submit"
-                               className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
-                    </form>
-                </Modal>
-                <h2 className="text-2xl font-bold mb-2">Programari viitoare</h2>
-                <ul>
-                    {upcomingAppointments.map(appointment => (
-                        <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
-                            <AppointmentMedic appointment={appointment}/>
-                            <button
-                                onClick={() => {
-                                    setIdConsultatie(appointment.idConsultatie);
-                                    openEditModal();
-                                }}
-                                className="bg-sky-500 hover:bg-sky-600 text-white rounded px-2.5 py-2 transition duration-200">Update
-                            </button>
-                            <Modal
-                                isOpen={editModalIsOpen}
-                                onRequestClose={closeEditModal}
-                                contentLabel="Update AppointmentMedic"
-                                className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center"
-                            >
-                                <form onSubmit={handleUpdateAppointment} className="flex flex-col">
-                                    <label className="mb-2">
-                                        Selectati data:
-                                        <input
-                                            type="date"
-                                            value={selectedDate}
-                                            onChange={(e) => setSelectedDate(e.target.value)}
-                                            required
-                                        />
-                                    </label>
-                                    <label className="mb-2">
-                                        Selectati ora:
-                                        <select
-                                            value={selectedHour}
-                                            onChange={(e) => setSelectedHour(e.target.value)}
-                                            required
-                                        >
-                                            {hoursOptions}
-                                        </select>
-                                    </label>
-                                    <input type="submit" value="Update"
-                                           className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
-                                </form>
-                            </Modal>
-                            <button onClick={() => deleteAppointment(appointment.idConsultatie)}
-                                    className="bg-red-500 text-white rounded px-2.5 py-2 hover:bg-red-700 ml-5 transition duration-200">Delete
-                            </button>
-                        </div>))}
-                </ul>
-            </div>
-        </div>
-    </div>)
+        </div>)
 };
 
 export default MedicAppointments;
--- medical_clinic_FRONTEND/my-app/src/pages/PatientAppointments.js
+++ medical_clinic_FRONTEND/my-app/src/pages/PatientAppointments.js
@@ -4,6 +4,12 @@ import PatientMenu from '../components/PatientMenu';
 import axios from "axios";
 import Modal from "react-modal";
 import AppointmentPatient from "../components/AppointmentPatient";
+import 'animate.css/animate.min.css';
+import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {toast} from "react-toastify";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const PatientAppointments = () => {
@@ -40,6 +46,18 @@ const PatientAppointments = () => {
         event.preventDefault();
         console.log('Updating appointment:', idConsultatie, selectedDate, selectedHour);
 
+        const date = new Date(selectedDate);
+        const currentDate = new Date();
+
+        if (date < currentDate) {
+            toast.error('Nu se pot actualiza programări la o dată din trecut');
+            return;
+        }
+        if (date.getDay() === 6 || date.getDay() === 0) {
+            toast.error('Nu se pot actualiza programări în weekend');
+            return;
+        }
+
         try {
             const response = await axios.put(`http://localhost:8081/api/consultatie/${idConsultatie}`, null, {
                 params: {
@@ -61,7 +79,6 @@ const PatientAppointments = () => {
         fetch(`http://localhost:8081/api/consultatie/${idConsultatie}`, {
             method: 'DELETE',
         }).then(() => {
-            // Refresh the appointments after deletion
             setPastAppointments(pastAppointments.filter(appointment => appointment.idConsultatie !== idConsultatie));
             setUpcomingAppointments(upcomingAppointments.filter(appointment => appointment.idConsultatie !== idConsultatie));
         });
@@ -91,68 +108,80 @@ const PatientAppointments = () => {
         setEditModalIsOpen(false);
     }
 
-    return (<div className="p-6">
-        <PatientMenu/>
-        <h1 className="text-3xl font-bold mb-4">Programari</h1>
-        <div className="flex flex-col md:flex-row">
-            <div className="bg-white p-4 rounded shadow w-full md:w-1/2 mr-2 mb-4 md:mb-0">
-                <h2 className="text-2xl font-bold mb-2">Programari anterioare</h2>
-                {pastAppointments.map(appointment => (
-                    <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
-                        <AppointmentPatient appointment={appointment}/>
-                    </div>))}
+    return (<div>
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            draggable
+            theme="light"
+        />
+        <div className="p-6">
+                <PatientMenu/>
+                <div className="flex flex-col md:flex-row mt-4">
+                    <div className="bg-white p-4 rounded shadow w-full md:w-1/2 mr-2 mb-4 md:mb-0">
+                        <h2 className="text-2xl font-bold mb-2">Programari anterioare</h2>
+                        {pastAppointments.map(appointment => (
+                            <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
+                                <AppointmentPatient appointment={appointment}/>
+                            </div>))}
+                    </div>
+                    <div className="bg-white p-4 rounded shadow w-full md:w-1/2">
+                        <h2 className="text-2xl font-bold mb-2">Programari viitoare</h2>
+                        <ul>
+                            {upcomingAppointments.map(appointment => (
+                                <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
+                                    <AppointmentPatient appointment={appointment}/>
+                                    <button
+                                        onClick={() => {
+                                            setIdConsultatie(appointment.idConsultatie);
+                                            openEditModal();
+                                        }}
+                                        className="bg-sky-500 hover:bg-sky-600 text-white rounded px-2.5 py-2 transition duration-200">
+                                        Update <FontAwesomeIcon icon={faEdit}/>
+                                    </button>
+                                    <Modal
+                                        isOpen={editModalIsOpen}
+                                        onRequestClose={closeEditModal}
+                                        contentLabel="Update AppointmentMedic"
+                                        className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center animate__animated animate__zoomIn"
+                                    >
+                                        <form onSubmit={handleUpdateAppointment} className="flex flex-col">
+                                            <label className="mb-2">
+                                                Selectati data:
+                                                <input
+                                                    type="date"
+                                                    value={selectedDate}
+                                                    onChange={(e) => setSelectedDate(e.target.value)}
+                                                    required
+                                                />
+                                            </label>
+                                            <label className="mb-2">
+                                                Selectati ora:
+                                                <select
+                                                    value={selectedHour}
+                                                    onChange={(e) => setSelectedHour(e.target.value)}
+                                                    required
+                                                >
+                                                    {hoursOptions}
+                                                </select>
+                                            </label>
+                                            <input type="submit" value="Update"
+                                                   className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
+                                        </form>
+                                    </Modal>
+                                    <button onClick={() => deleteAppointment(appointment.idConsultatie)}
+                                            className="bg-red-500 text-white rounded px-2.5 py-2 hover:bg-red-700 ml-5 transition duration-200">
+                                        Delete <FontAwesomeIcon icon={faTrash}/>
+                                    </button>
+                                </div>))}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className="bg-white p-4 rounded shadow w-full md:w-1/2">
-                <h2 className="text-2xl font-bold mb-2">Programari viitoare</h2>
-                <ul>
-                    {upcomingAppointments.map(appointment => (
-                        <div key={appointment.idConsultatie} className="border-sky-500 border-2 mb-1 p-2">
-                            <AppointmentPatient appointment={appointment}/>
-                            <button
-                                onClick={() => {
-                                    setIdConsultatie(appointment.idConsultatie);
-                                    openEditModal();
-                                }}
-                                className="bg-sky-500 hover:bg-sky-600 text-white rounded px-2.5 py-2 transition duration-200">Update
-                            </button>
-                            <Modal
-                                isOpen={editModalIsOpen}
-                                onRequestClose={closeEditModal}
-                                contentLabel="Update AppointmentMedic"
-                                className="w-80 h-80 p-4 m-4 md:w-1/2 md:h-1/2 lg:w-1/3 lg:h-2/3 mx-auto mt-36 bg-indigo-300 rounded-2xl p-5 border-2 border-indigo-700 text-center content-center"
-                            >
-                                <form onSubmit={handleUpdateAppointment} className="flex flex-col">
-                                    <label className="mb-2">
-                                        Selectati data:
-                                        <input
-                                            type="date"
-                                            value={selectedDate}
-                                            onChange={(e) => setSelectedDate(e.target.value)}
-                                            required
-                                        />
-                                    </label>
-                                    <label className="mb-2">
-                                        Selectati ora:
-                                        <select
-                                            value={selectedHour}
-                                            onChange={(e) => setSelectedHour(e.target.value)}
-                                            required
-                                        >
-                                            {hoursOptions}
-                                        </select>
-                                    </label>
-                                    <input type="submit" value="Update"
-                                           className="mt-7 border-2 border-indigo-700 rounded-3xl w-1/2 mx-auto"/>
-                                </form>
-                            </Modal>
-                            <button onClick={() => deleteAppointment(appointment.idConsultatie)}
-                                    className="bg-red-500 text-white rounded px-2.5 py-2 hover:bg-red-700 ml-5 transition duration-200">Delete
-                            </button>
-                        </div>))}
-                </ul>
-            </div>
-        </div>
-    </div>);
+        </div>);
 }
 
 export default PatientAppointments;
