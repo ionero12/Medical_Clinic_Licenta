14d98271024258a62b3fa83cce7bed0928ea3260
Ionela Alexandra <sauionela51@gmail.com>
2024-04-23 19:38:09 +0300

    implemented feedback mechanism

--- medical_clinic_FRONTEND/my-app/src/pages/Medic.js
+++ medical_clinic_FRONTEND/my-app/src/pages/Medic.js
@@ -1,13 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import PacientMenu from "../components/PatientMenu";
+import StarRating from "../components/StarRating";
 
 //TODO: adaugat mai multe detalii(nu stiu de unde) si programu medicului facut mai vizual
-//TODO: de adaugat experienta si universitatea la medic
-//TODO: de implementat ratingurile si feedbackurile(api la consultatie/medic si apoi afisat pe pagina rating si feedback)
 
 const Medic = () => {
     const [medic, setMedic] = useState(null);
+    const [consultatii, setConsultatii] = useState([]);
     const {idMedic} = useParams();
 
     useEffect(() => {
@@ -19,25 +19,70 @@ const Medic = () => {
             });
     }, [idMedic]);
 
+    useEffect(() => {
+        fetch(`http://localhost:8081/api/consultatie/medic?idMedic=${idMedic}`)
+            .then(response => response.json())
+            .then(data => {
+                setConsultatii(data);
+                console.log(data);
+            });
+    }, [idMedic]);
+
     if (!medic) {
         return <div className="flex justify-center items-center h-screen">Loading...</div>;
     }
 
     return (<div className="p-6">
         <PacientMenu/>
-        <div className="flex flex-col md:flex-row mt-4">
+        <h2 className="text-3xl font-bold mb-4 text-gray-400">{medic.numeMedic} {medic.prenumeMedic}</h2>
+        <div className="flex flex-col md:flex-row">
             <div className="bg-white p-4 rounded shadow w-full md:w-1/2 mr-2 mb-4 md:mb-0 text-lg">
-                <h2 className="text-3xl font-bold mb-4">{medic.numeMedic} {medic.prenumeMedic}</h2>
                 <p className="mb-2"><strong>Email:</strong> {medic.emailMedic}</p>
                 <p className="mb-2"><strong>Telefon:</strong> {medic.telefonMedic}</p>
-                <div className="mb-2">
-                    <strong>Program:</strong>
-                    <div className="mt-2 bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4" role="alert">
-                        <p className="font-bold">Luni-Vineri:</p>
-                        <p>08:00-18:00</p>
-                    </div>
+                <p className="mb-2"><strong>Universitate:</strong> {medic.universitate}</p>
+                <p className="mb-2"><strong>Experienta:</strong> {medic.experienta} ani in domeniu</p>
+                <div className="mb-4">
+                    <table className="table-auto">
+                        <thead>
+                        <tr>
+                            <th className="border-2 border-gray-400 px-4 py-2">Zi</th>
+                            <th className="border-2 border-gray-400 px-4 py-2">Program</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td className="border-2 border-gray-400 px-4 py-2 font-semibold">Luni</td>
+                            <td className="border-2 border-gray-400 px-4 py-2">08:00-18:00</td>
+                        </tr>
+                        <tr>
+                            <td className="border-2 border-gray-400 px-4 py-2 font-semibold">Marți</td>
+                            <td className="border-2 border-gray-400 px-4 py-2">08:00-18:00</td>
+                        </tr>
+                        <tr>
+                            <td className="border-2 border-gray-400 px-4 py-2 font-semibold">Miercuri</td>
+                            <td className="border-2 border-gray-400 px-4 py-2">08:00-18:00</td>
+                        </tr>
+                        <tr>
+                            <td className="border-2 border-gray-400 px-4 py-2 font-semibold">Joi</td>
+                            <td className="border-2 border-gray-400 px-4 py-2">08:00-18:00</td>
+                        </tr>
+                        <tr>
+                            <td className="border-2 border-gray-400 px-4 py-2 font-semibold">Vineri</td>
+                            <td className="border-2 border-gray-400 px-4 py-2">08:00-18:00</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
+            <div className="bg-white p-4 rounded shadow w-full md:w-1/2">
+                {consultatii.filter(consultatie => consultatie.rating !== null).map((consultatie, index) => (
+                    <div key={index} className="p-4 border-2 border-blue-600 rounded-3xl mb-4">
+                        <StarRating rating={consultatie.rating}/>
+                        <p>{consultatie.feedback}</p>
+                    </div>
+                ))}
+            </div>
+
         </div>
     </div>);
 };
