a8dce27ef37e71f088d5bbabcc97afb4cb488218
Ionela Alexandra <sauionela51@gmail.com>
2024-05-11 19:53:03 +0300

    worked at the ui

--- medical_clinic_FRONTEND/my-app/src/pages/PatientProfile.js
+++ medical_clinic_FRONTEND/my-app/src/pages/PatientProfile.js
@@ -44,9 +44,47 @@ function PatientProfile() {
     return (<div className="p-6">
             <PatientMenu/>
             <div className="pt-5 sm:px-6 lg:px-8 flex flex-col items-center justify-center min-h-screen">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="bg-white grid md:grid-cols-3 grid-cols-1 gap-4 p-2">
+                    <div className="flex flex-col max-w-md p-5 shadow-md mt-4 md:mt-0 w-full border border-gray-400">
+                        <label className="block mb-2">
+                            Email:
+                            <input
+                                type="email"
+                                value={emailPacient}
+                                onChange={e => setEmailPacient(e.target.value)}
+                                className="w-full px-2 py-1 mb-2 border border-gray-400"
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Password:
+                            <input
+                                type="password"
+                                value={parolaPacient}
+                                onChange={e => setParolaPacient(e.target.value)}
+                                className="w-full px-2 py-1 mb-2 border border-gray-400"
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Phone number:
+                            <input
+                                type="phone"
+                                value={telefonPacient}
+                                onChange={e => setTelefonPacient(e.target.value)}
+                                className="w-full px-2 py-1 mb-2 border border-gray-400"
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Age:
+                            <input
+                                type="number"
+                                value={varstaPacient}
+                                onChange={e => setVarstaPacient(e.target.value)}
+                                className="w-full px-2 py-1 mb-2 border border-gray-400"
+                            />
+                        </label>
+                    </div>
                     <div
-                        className="flex flex-col items-center max-w-md bg-white p-5 rounded shadow-md mb-4 md:mb-0 w-full">
+                        className="flex flex-col items-center max-w-md p-5 shadow-md mb-4 md:mb-0 w-full border border-gray-400">
                         <h2 className="text-2xl p-3">{numePacient} {prenumePacient}</h2>
                         <div
                             className="w-32 h-32 rounded-full border border-gray-400 flex items-center justify-center mb-4">
@@ -66,26 +104,14 @@ function PatientProfile() {
                                 <option value="N">No</option>
                             </select>
                         </label>
-                        <label className="block mb-2">
-                            Email:
-                            <input
-                                type="email"
-                                value={emailPacient}
-                                onChange={e => setEmailPacient(e.target.value)}
-                                className="w-full px-2 py-1 mb-2 border border-gray-400"
-                            />
-                        </label>
-                        <label className="block mb-2">
-                            Password:
-                            <input
-                                type="password"
-                                value={parolaPacient}
-                                onChange={e => setParolaPacient(e.target.value)}
-                                className="w-full px-2 py-1 mb-2 border border-gray-400"
-                            />
-                        </label>
+                        <div className="flex justify-center items-end text-lg w-full">
+                            <button onClick={updatePacientProfile}
+                                    className="mt-4 mb-4 p-3 bg-blue-700 hover:bg-blue-900 text-white cursor-pointer rounded">Update
+                                Profile
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex flex-col max-w-md bg-white p-5 rounded shadow-md mt-4 md:mt-0 w-full">
+                    <div className="flex flex-col max-w-md p-5 shadow-md mt-4 md:mt-0 w-full border border-gray-400">
                         <label className="block mb-2">
                             Second name:
                             <input
@@ -122,36 +148,10 @@ function PatientProfile() {
                                 className="w-full px-2 py-1 mb-2 border border-gray-400"
                             />
                         </label>
-                        <label className="block mb-2">
-                            Age:
-                            <input
-                                type="number"
-                                value={varstaPacient}
-                                onChange={e => setVarstaPacient(e.target.value)}
-                                className="w-full px-2 py-1 mb-2 border border-gray-400"
-                            />
-                        </label>
-
-                        <label className="block mb-2">
-                            Phone number:
-                            <input
-                                type="phone"
-                                value={telefonPacient}
-                                onChange={e => setTelefonPacient(e.target.value)}
-                                className="w-full px-2 py-1 mb-2 border border-gray-400"
-                            />
-                        </label>
                     </div>
                 </div>
-                <div className="flex justify-center">
-                    <button onClick={updatePacientProfile}
-                            className="mt-4 px-4 py-2 bg-gray-300 text-black border-none cursor-pointer">Update Profile
-                    </button>
-                </div>
             </div>
-        </div>
-
-    );
+        </div>);
 }
 
 export default PatientProfile;
