7508e6166f6f476763b4c645d94d2763c5fbf91e
Ionela Alexandra <sauionela51@gmail.com>
2024-06-08 23:36:37 +0300

    Update Prices.js

--- medical_clinic_FRONTEND/my-app/src/pages/Prices.js
+++ medical_clinic_FRONTEND/my-app/src/pages/Prices.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import PatientMenu from "../components/PatientMenu";
-import api from '../user/api.js'
+import {api} from '../user/api.js'
 
 
 const Prices = () => {
